61e519e64456214686a0f440291a88e98e28d1cd
UBC Student <xuanmaii@pender.students.cs.ubc.ca>
2024-03-12 21:51:27 -0700

    add Room interface

---
+++ src/type/Room.ts
@@ -0,0 +1,15 @@
+export interface Room {
+	fullname: string; // full building name
+	shortname: string; // short building name
+	number: string; // room number
+	name: string; // room id (rooms_shortname + _ + rooms_number)
+	address: string; // building address
+	lat: number; // lat of the building
+	lon: number; // lon of building
+	seats: number; // number of seats
+	type: string; // room type
+	furniture: string; // room furniture
+	href: string; // link to full details onl
+}
+
+
