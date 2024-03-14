132d4e89d1fc8f6a1e42271c4ee4f89610e5e3db
kmelocot <kmelocot@students.cs.ubc.ca>
2024-03-14 00:39:59 -0700

    add room interface

---
+++ src/type/Room.ts
@@ -0,0 +1,13 @@
+export interface Room {
+	fullname: string;
+	shortname: string;
+	number: string;
+	name: string;
+	address: string;
+	lat: number;
+	lon: number;
+	seats: number;
+	type: string;
+	furniture: string;
+	href: string;
+}
